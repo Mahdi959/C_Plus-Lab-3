e9b86cb0b452c65129e262593496492e7dca3a1e
Mahdi Cloud9 <ashrafeem8@gmail.com>
2022-06-04 12:12:48 +0000

    Default code from Professor added by Mahdi

---
+++ Driver.cpp
@@ -0,0 +1,44 @@
+// Complete the program given by writing the definition of the 'addSize' function
+// (it is being called in line  25)
+// Also, add code, starting at line 29, to fill in the extra space created in the array
+//  before it is displayed at the end
+
+#include<iostream>
+
+using namespace std;
+
+//function prototype goes here
+
+
+
+int main()
+{
+	int SIZE = 10, currentSize;
+	int *arrPtr = new int[SIZE];
+
+	for (int i = 0; i < SIZE; i++)   //putting values in the array
+		arrPtr[i] = i;
+
+	currentSize = SIZE;			//why is this here, what is it for?? (might be useful later)
+
+	//What does the function call in the next line tell you about the function??
+	arrPtr = addSize(arrPtr, SIZE);   	//the array is now bigger, with the original elements in it. SIZE now has the new size
+
+
+	//ADD VALUES TO THE ARRAY TO FILL IN THE EXTRA SPACE CREATED
+
+
+
+
+
+
+	cout << "The new array is: ";
+	// This just displays the array
+	for (int i = 0; i < SIZE; i++)
+		cout << arrPtr[i]<<' ';
+
+	return 0;
+}
+
+//function definition goes here
+ 
